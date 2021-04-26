5f6105d50f41b5ceaee8430f3d7c15e2de65017a
Procoder16 <smsoumik1608@gmail.com>
2021-04-26 20:40:12 +0530

    theme data changed

--- messiah_app/lib/themes/app_color.dart
+++ messiah_app/lib/themes/app_color.dart
@@ -5,8 +5,8 @@ class AppColor {
   static Color textColorDark = const Color(0xffffffff);
 
   static Color bodyColor = const Color(0xffffffff);
-  static Color bodyColorDark = const Color(0xff0E0E0F);
+  static Color bodyColorDark = const Color(0xFF0B1328);
 
   static Color buttonBackgroundColor = const Color(0xffF7F7F7);
-  static Color buttonBackgroundColorDark = const Color(0xff121212);
+  static Color buttonBackgroundColorDark = const Color(0xFF1D2740);
 }
