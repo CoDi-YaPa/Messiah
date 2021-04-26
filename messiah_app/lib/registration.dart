52fb9ccce9abae090df9c27d65c39868bc59ca4c
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2021-04-26 20:13:52 +0530

    added login and signup Page

---
+++ messiah_app/lib/registration.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+class SecondRoute extends StatefulWidget {
+  @override
+  _SecondRouteState createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final double fontScale = MediaQuery.of(context).textScaleFactor;
+    return Scaffold(
+        body: SingleChildScrollView(
+            child:Container(
+                width: size.width,
+                height: size.height,
+                padding: EdgeInsets.only(left: 20, right: 20, top: size.height * 0.14, bottom: size.height * 0.06),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Hey , \nNew User", style: Theme.of(context).textTheme.headline1.copyWith(fontSize: size.width * 0.1,)),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColorLight,
+                                  borderRadius: BorderRadius.all(Radius.circular(20))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Name",
+
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20,),
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColorLight,
+                                  borderRadius: BorderRadius.all(Radius.circular(20))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: " E-mail",
+
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20,),
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColorLight,
+                                  borderRadius: BorderRadius.all(Radius.circular(20))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Phone Number",
+
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20,),
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColorLight,
+                                  borderRadius: BorderRadius.all(Radius.circular(20))
+                              ),
+                              child: TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Password",
+
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 70,),
+                            Column(
+                                children: [
+                                  MaterialButton(
+                                    onPressed: () => {
+
+                                    },
+                                    elevation: 100,
+                                    padding: EdgeInsets.all(18),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20)
+                                    ),
+                                    color: Colors.white,
+                                    child: Center(child: Text("Sign up", style: TextStyle(fontWeight: FontWeight.bold),)),
+                                  ),
+                                  SizedBox(
+                                    height: 50,
+                                  ),
+
+                                ])
+                          ]),
+
+
+                    ]))));
+  }
+}
