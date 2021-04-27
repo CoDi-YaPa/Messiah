392c69e2cb01ec57c7406025ec24bf66fb172e5a
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2021-04-27 20:46:29 +0530

    added rough bottom navigation bar will implement page change later

--- messiah_app/lib/Screens/FrontPage.dart
+++ messiah_app/lib/Screens/FrontPage.dart
@@ -1,7 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:messiah_app/Models/dataModel.dart';
 import 'package:messiah_app/themes/app_color.dart';
+
 
 class FrontPage extends StatefulWidget {
   @override
@@ -14,6 +16,43 @@ class _FrontPageState extends State<FrontPage> {
   double scaleFactor = 1.0;
   double borderRadius = 1.0;
   bool isDrawerOpen = false;
+
+
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+
+  List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Settings',
+      style: optionStyle,
+    ),
+  ];
+
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -69,6 +108,7 @@ class _FrontPageState extends State<FrontPage> {
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,color: AppColor.textColorDark),
                   ),
                 ),
+                _widgetOptions.elementAt(_selectedIndex),
                 SizedBox(
                   width: 30,
                 )
@@ -127,7 +167,32 @@ class _FrontPageState extends State<FrontPage> {
                   );
                 },
               ),
-            )
+            ),
+            BottomNavigationBar(
+
+              items:const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.drop_fill),
+                label: 'Blood',
+                backgroundColor: Colors.red,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.drop_triangle),
+                label: 'Plasma',
+                backgroundColor: Colors.green,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.bed_double_fill),
+                label: 'Oxygen',
+                backgroundColor: Colors.purple,
+              ),
+
+            ],
+              currentIndex: _selectedIndex,
+              selectedItemColor: AppColor.buttonBackgroundColorDark,
+              onTap: _onItemTapped,
+            backgroundColor:AppColor.textFieldColorDark ,
+            elevation: 0,)
 
           ],
         ),
