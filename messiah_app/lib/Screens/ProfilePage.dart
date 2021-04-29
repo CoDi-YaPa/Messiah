de1565672a4d314ceae89d37dc5357c08615daaa
Procoder16 <smsoumik1608@gmail.com>
2021-04-29 18:01:35 +0530

    made the profile page

--- messiah_app/lib/Screens/ProfilePage.dart
+++ messiah_app/lib/Screens/ProfilePage.dart
@@ -24,7 +24,84 @@ class _ProfilePageState extends State<ProfilePage> {
           child: Container(
             child: Column(
               children: [
-                CircleAvatar(),
+                SizedBox(
+                  height: 20.0,
+                ),
+                CircleAvatar(
+                  radius: 75.0,
+                  backgroundImage: AssetImage('images/contacts.png'),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Card(
+                  margin: EdgeInsets.only(top: 30.0, left: 15.0, right: 15.0),
+                  child: ListTile(
+                    tileColor: Color(0xFF1D2740),
+                    leading: Text(
+                      'Name',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blue[300],
+                      ),
+                    ),
+                    trailing: Text(
+                      'Soumik Mukherjee',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.only(top: 30.0, left: 15.0, right: 15.0),
+                  child: ListTile(
+                    tileColor: Color(0xFF1D2740),
+                    leading: Text(
+                      'Phone No.',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blue[300],
+                      ),
+                    ),
+                    trailing: Text(
+                      '700XXXXXXX',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.only(top: 30.0, left: 15.0, right: 15.0),
+                  child: ListTile(
+                    tileColor: Color(0xFF1D2740),
+                    leading: Text(
+                      'Address',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blue[300],
+                      ),
+                    ),
+                    trailing: Text(
+                      'Kolkata, West Bengal',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.only(top: 30.0, left: 15.0, right: 15.0),
+                  child: ListTile(
+                    tileColor: Color(0xFF1D2740),
+                    leading: Text(
+                      'Blood Group',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blue[300],
+                      ),
+                    ),
+                    trailing: Text(
+                      'A(-)',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
