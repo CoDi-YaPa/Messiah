48e8a2517144712c7088f766c8031579e9900349
Procoder16 <smsoumik1608@gmail.com>
2021-04-30 19:13:34 +0530

    added my slider template

--- messiah_app/lib/CustomWidgets/donarContainer.dart
+++ messiah_app/lib/CustomWidgets/donarContainer.dart
@@ -1,18 +1,19 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:messiah_app/themes/app_color.dart';
 
 class DonarConatinerBlood extends StatelessWidget {
- final String name;
+  final String name;
   final String bloodGroup;
   final String location;
   final String phoneNumber;
- final String email;
+  final String email;
 
-  DonarConatinerBlood({this.name,this.bloodGroup,this.location,this.phoneNumber,this.email});
-
-
+  DonarConatinerBlood(
+      {this.name,
+      this.bloodGroup,
+      this.location,
+      this.phoneNumber,
+      this.email});
 
   @override
   Widget build(BuildContext context) {
@@ -20,40 +21,38 @@ class DonarConatinerBlood extends StatelessWidget {
       height: 130,
       margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        borderRadius:BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(20),
         color: AppColor.bodyColorDark,
       ),
-
       child: Row(
         children: [
           SizedBox(
             width: 10,
           ),
-
-          CircleAvatar(radius: 60,),
-          SizedBox(width: 20,),
+          CircleAvatar(
+            radius: 60,
+          ),
+          SizedBox(
+            width: 20,
+          ),
           Column(
             children: [
-              SizedBox(height: 5,),
-              Text(name??'Example Name',style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold
-              ),),
-              Text(bloodGroup??'**',style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold
-              )),
-              Text(location??'Example Place'),
-              Text(phoneNumber??'98XXXXXXXXXXXX'),
-              Text(email??'vuXXXXXXXXX@gmail.com'),
-
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                name ?? 'Example Name',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Text(bloodGroup ?? '**',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              Text(location ?? 'Example Place'),
+              Text(phoneNumber ?? '98XXXXXXXXXXXX'),
+              Text(email ?? 'vuXXXXXXXXX@gmail.com'),
             ],
-
           )
-
         ],
       ),
-
     );
   }
 }
--- messiah_app/lib/main.dart
+++ messiah_app/lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:messiah_app/Intro_sliderPage.dart';
 import 'loginPage.dart';
 import 'package:messiah_app/themes/app_theme.dart';
+
 void main() {
-  runApp(MaterialApp(
-      home:MyApp(),
-      theme: AppTheme.dark,),);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+      theme: AppTheme.dark,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
+    return IntroSliderPage();
   }
 }
-
